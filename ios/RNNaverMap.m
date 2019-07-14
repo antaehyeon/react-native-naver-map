57c158cd4cfacffe4285533fc93ad9057247aa9f
Hyeon <antaehyeon@live.co.kr>
2019-07-15 04:02:08 +0900

    Feat: IOS 쪽 네이버 맵 초기코드 설정

--- ios/RNNaverMap.m
+++ ios/RNNaverMap.m
@@ -1,13 +1,33 @@
+// RNNaverMap.m
+#import <React/RCTLog.h>
+#import <MapKit/MapKit.h>
+#import <React/RCTViewManager.h>
+@import NMapsMap;
 
-#import "RNNaverMap.h"
+@interface RNNaverMap : RCTViewManager
+@end
 
 @implementation RNNaverMap
 
-- (dispatch_queue_t)methodQueue
+// To export a module named RNNaverMap
+RCT_EXPORT_MODULE(RNNaver);
+
+- (UIView *)view
 {
-    return dispatch_get_main_queue();
+//   return [[MKMapView alloc] init];
+    // NMFMapView *mapView = [[NMFMapView alloc] initWithFrame:self.view.frame];
+    // [self.view addSubview:mapView];
+    // return mapView;
+
+    return [[NMFMapView alloc] init];
 }
-RCT_EXPORT_MODULE()
+
+RCT_EXPORT_METHOD(addEvent:(NSString *)name location:(NSString *)location)
+{
+  RCTLogInfo(@"Pretending to create an event %@ at %@", name, location);
+}
+
+// This would name the module AwesomeCalendarManager instead
+// RCT_EXPORT_MODULE(AwesomeCalendarManager);
 
 @end
-  
